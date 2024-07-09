da4d2aa43763a3d63842959514dbab5581adacd8
Sakshi KC <kcsakshi92@gmail.com>
2024-07-09 19:44:21 +0545

    component to addtodo

---
+++ list-it/src/todoform.js
@@ -0,0 +1,20 @@
+import react from "react";
+import { useState } from "react";
+import './todoform.css';
+
+function TodoForm({addTodo}){
+    const [value,setValue]=useState('');
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(!value) return;
+        addTodo (value);
+        setValue('');
+    };
+return(
+    <form onSubmit={handleSubmit} className="todo-form">
+        <input type="text" className="input" value={value} placeholder="Add a new task" onChange={e=>setValue(e.target.value)}/>
+        <button type="submit" className="button">Add</button>
+    </form>
+);
+}
+export default TodoForm;
